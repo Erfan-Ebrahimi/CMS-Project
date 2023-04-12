d776e8b445e62765c967d5978cf768c153efa9f8
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-13 04:19:49 +0430

    Sidebar developed

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,17 +1,24 @@
 import './Sidebar.scss';
 
+// ----------ICONS--------------//
+import {AiOutlineHome} from 'react-icons/ai' 
+import {MdProductionQuantityLimits} from 'react-icons/md' 
+import {BiCommentDetail} from 'react-icons/bi' 
+import {FiUsers} from 'react-icons/fi' 
+import {BsBagCheck} from 'react-icons/bs' 
+import {BsCurrencyDollar} from 'react-icons/bs' 
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <h1 className='sidebar-title'>به داشبور خوش آمدید</h1>
+        <h1 className='sidebar-title'>به داشبورد خوش آمدید</h1>
         <ul className='sidebar-links'>
-            <li><a href="#1">خانه</a></li>
-            <li><a href="#1">محصولات</a></li>
-            <li><a href="#1">نظرات</a></li>
-            <li><a href="#1">کاربران</a></li>
-            <li><a href="#1">سفارشات</a></li>
-            <li><a href="#1">تخفیف ها</a></li>
+            <li className='active'><a href="#1"><AiOutlineHome className='icon'/>خانه</a></li>
+            <li><a href="#1"><MdProductionQuantityLimits className='icon'/> محصولات</a></li>
+            <li><a href="#1"><BiCommentDetail className='icon'/>نظرات</a></li>
+            <li><a href="#1"><FiUsers className='icon'/>کاربران</a></li>
+            <li><a href="#1"><BsBagCheck className='icon'/>سفارشات</a></li>
+            <li><a href="#1"><BsCurrencyDollar className='icon'/>تخفیف ها</a></li>
         </ul>
 
     </div>
