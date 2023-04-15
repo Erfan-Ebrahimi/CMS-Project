666538b7de17f328158c1934a7cbfb5c08b12191
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-15 07:40:18 +0430

    ProductsTable primary developed

---
+++ src/components/products/productsTable/ProductsTable.js
@@ -0,0 +1,32 @@
+import './ProductsTable.scss';
+
+import img1 from '../../../images/prof.jfif';
+
+const ProductsTable = () => {
+  return (
+    <table className='products-table'>
+        <tr className='pt-heading-tr'>
+            <th>عکس</th>
+            <th>نام</th>
+            <th>قیمت</th>
+            <th>موجودی</th>
+            <th>تغییرات</th>
+        </tr>
+        <tr>
+            <td>
+                <img className='pt-table' src={img1} alt="img" />
+            </td>
+            <td>روغن</td>
+            <td>92000 تومان</td>
+            <td>85</td>
+            <td>
+                <button className='pt-btn'>جزییات</button>
+                <button className='pt-btn'>حذف</button>
+                <button className='pt-btn'>ویرایش</button>
+            </td>
+        </tr>
+    </table>
+  )
+}
+
+export default ProductsTable
