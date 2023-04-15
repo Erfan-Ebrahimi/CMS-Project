ea7d5185cac77ffa0194eca27a3af1f51411a4c5
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-15 23:31:01 +0430

    COM ProductsTable & .scss Changed

--- src/components/products/productsTable/ProductsTable.js
+++ src/components/products/productsTable/ProductsTable.js
@@ -1,31 +1,40 @@
 import './ProductsTable.scss';
-
+import { Table } from 'react-bootstrap';
 import img1 from '../../../images/prof1.jfif';
+
+// -------------ICONS------------//
+import {GrCircleInformation} from 'react-icons/gr'
+import {RiDeleteBin6Line} from 'react-icons/ri'
+import {AiOutlineEdit} from 'react-icons/ai'
 
 const ProductsTable = () => {
   return (
-    <table className='products-table'>
-        <tr className='pt-heading-tr'>
+    <Table bordered hover className='products-table'>
+      <thead>
+        <tr className='pt-heading'>
             <th>عکس</th>
             <th>نام</th>
             <th>قیمت</th>
             <th>موجودی</th>
             <th>تغییرات</th>
         </tr>
-        <tr>
-            <td>
-                <img className='pt-table' src={img1} alt="img" />
-            </td>
-            <td>روغن</td>
-            <td>92000 تومان</td>
-            <td>85</td>
-            <td>
-                <button className='pt-btn'>جزییات</button>
-                <button className='pt-btn'>حذف</button>
-                <button className='pt-btn'>ویرایش</button>
-            </td>
+      </thead>
+      <tbody >
+        <tr className='pt-body'>
+          <td className="align-middle">
+              <img className='pt-img' src={img1} alt="img" />
+          </td>
+          <td className="align-middle">روغن</td>
+          <td className="align-middle">92000 تومان</td>
+          <td className="align-middle">85</td>
+          <td className="align-middle">
+              <button className='pt-btn btn btn-warning'><GrCircleInformation/></button>
+              <button className='pt-btn btn btn-success'><AiOutlineEdit/></button>
+              <button className='pt-btn btn btn-danger'><RiDeleteBin6Line/></button>
+          </td>
         </tr>
-    </table>
+      </tbody>
+    </Table>
   )
 }
 
