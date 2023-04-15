0ea3c65a7f42ac015c13589ab698139db442d9e1
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-15 12:03:51 +0430

    Routes changed

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
+import Comments from './components/comments/Comments';
+import Products from './components/products/Products';
+import Users from './components/users/Users';
+import Orders from './components/orders/Orders';
+import Offs from './components/offs/Offs';
+
 const routes = [
     {path:'/products' , element: <Products/>},
-    {path:'/comments' , element: <Comment/>},
+    {path:'/comments' , element: <Comments/>},
     {path:'/users' , element: <Users/>},
     {path:'/orders' , element: <Orders/>},
     {path:'/offs' , element: <Offs/>},
