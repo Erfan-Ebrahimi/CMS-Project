f87d7c5bbaa0f73ea0f1bdf23dfcb4d661435a37
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-16 07:56:37 +0430

    logic the delete btn in productsTable & deleteModal

--- src/components/modals/deleteModal/DeleteModal.js
+++ src/components/modals/deleteModal/DeleteModal.js
@@ -2,19 +2,20 @@ import ReactDOM from 'react-dom';
 import './DeleteModal.scss';
 
 
-const DeleteModal = () => {
+const DeleteModal = ({submitAction , cancelAction}) => {
   return ReactDOM.createPortal(
     <div className="modal-parent active">
         <div className="delete-modal">
             <h1>آیا از حذف اطمینان دارید ؟</h1>
             <div className="dm-btns">
-                <button className='dm-btn btn btn-danger'>بله</button>
-                <button className='dm-btn btn btn-success'>خیر</button>
+                <button className='dm-btn btn btn-danger' onClick={() => submitAction()}>بله</button>
+                <button className='dm-btn btn btn-success' onClick={() => cancelAction()}>خیر</button>
             </div>
         </div>
     </div> , document.getElementById('modals-parent')
   )
 }
 
-// class activero zamani midim ke karbar vared shod
+// class activero zamani midim ke karbar dar productsTable click kard roy btns
+// from prop sub & cancel ro migirim -> from productsTable
 export default DeleteModal;
--- src/components/products/productsTable/ProductsTable.js
+++ src/components/products/productsTable/ProductsTable.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import './ProductsTable.scss';
 import { Table } from 'react-bootstrap';
 import img1 from '../../../images/prof1.jfif';
+
+// -----------COMPONENTS---------//
+import DeleteModal from '../../modals/deleteModal/DeleteModal';
 
 // -------------ICONS------------//
 import {GrCircleInformation} from 'react-icons/gr'
@@ -8,33 +12,50 @@ import {RiDeleteBin6Line} from 'react-icons/ri'
 import {AiOutlineEdit} from 'react-icons/ai'
 
 const ProductsTable = () => {
+
+  const [isShowDeleteModal , setIsShowDeleteModal] = useState(false)
+
+  const deleteModalSubmitAction = () => {
+    console.log('red')
+    setIsShowDeleteModal(false)
+  }
+
+  const deleteModalCancelAction = () => {
+    console.log('blue')
+    setIsShowDeleteModal(false)
+  }
+
   return (
-    <Table bordered hover className='products-table'>
-      <thead>
-        <tr className='pt-heading'>
-            <th>عکس</th>
-            <th>نام</th>
-            <th>قیمت</th>
-            <th>موجودی</th>
-            <th>تغییرات</th>
-        </tr>
-      </thead>
-      <tbody >
-        <tr className='pt-body'>
-          <td className="align-middle">
-              <img className='pt-img' src={img1} alt="img" />
-          </td>
-          <td className="align-middle">روغن</td>
-          <td className="align-middle">92000 تومان</td>
-          <td className="align-middle">85</td>
-          <td className="align-middle">
-              <button className='pt-btn btn btn-warning'><GrCircleInformation/></button>
-              <button className='pt-btn btn btn-success'><AiOutlineEdit/></button>
-              <button className='pt-btn btn btn-danger'><RiDeleteBin6Line/></button>
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <Table bordered hover className='products-table'>
+        <thead>
+          <tr className='pt-heading'>
+              <th>عکس</th>
+              <th>نام</th>
+              <th>قیمت</th>
+              <th>موجودی</th>
+              <th>تغییرات</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr className='pt-body'>
+            <td className="align-middle">
+                <img className='pt-img' src={img1} alt="img" />
+            </td>
+            <td className="align-middle">روغن</td>
+            <td className="align-middle">92000 تومان</td>
+            <td className="align-middle">85</td>
+            <td className="align-middle">
+                <button className='pt-btn btn btn-warning'><GrCircleInformation/></button>
+                <button className='pt-btn btn btn-success' onClick={() => setIsShowDeleteModal(true)}><AiOutlineEdit/></button>
+                <button className='pt-btn btn btn-danger'><RiDeleteBin6Line/></button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      { isShowDeleteModal && <DeleteModal submitAction={deleteModalSubmitAction} cancelAction={deleteModalCancelAction} />}
+    </>
   )
 }
 
