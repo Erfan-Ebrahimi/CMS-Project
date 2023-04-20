cb76399379860c4ebf332497bc2421d8cd9a6321
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-04-21 02:52:18 +0430

    add children & remove </Table> in DetailsModal.js

--- FrontEnd/src/components/modals/detailsModal/DetailsModal.js
+++ FrontEnd/src/components/modals/detailsModal/DetailsModal.js
@@ -1,50 +1,17 @@
-import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Table } from 'react-bootstrap';
 import './DetailsModal.scss';
 
 //hideModal az ProductsTable miad
-const DetailsModal = ({ hideModal }) => {
-
-    useEffect(() => {
-        const closed = (event) => {
-            // 27 => ESCAPE in keyboard
-            if (event.keyCode === 27){
-                hideModal()
-            }
-        }
-        
-        window.addEventListener('keydown' , closed)
-
-        //baray jelogiri az tadakhol event anra bad az unMouting remove mikonim
-        //baray faze unMouting dar useEffect bayad return anjam dahim
-
-        return () => window.removeEventListener('keydown' , closed)
-    })
+const DetailsModal = ({ children }) => {
 
   return ReactDOM.createPortal(
       <div className='modal-parent active'>
         <div className="details-modal">
-            <button type="button" className="btn-close" aria-label="Close" onClick={hideModal}></button>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>نام محصول</th>
-                        <th>قیمت</th>
-                        <th>موجودی</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>روغن نباتی</td>
-                        <td>100000 تومان</td>
-                        <td>50</td>
-                    </tr>
-                </tbody>
-            </Table>
+            {/* Table ra dar ProductsTable misazim v be inja mifrestim */}
+            {children}
         </div>
     </div> , document.getElementById('modals-parent')
   )
 }
 
-export default DetailsModal
+export default DetailsModal;
