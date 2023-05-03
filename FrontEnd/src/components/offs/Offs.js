eedaf6ba260fce3f6100931e911debbadac29e20
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-05-03 18:22:08 +0430

    logic acceptModal for offs.js

--- FrontEnd/src/components/offs/Offs.js
+++ FrontEnd/src/components/offs/Offs.js
@@ -2,14 +2,12 @@ import { useState , useEffect } from 'react';
 import './Offs.scss';
 
 // ---------------MUI & BOOTSTRAP-------------------//
-import { TextField , Box } from '@mui/material';
 import {Table} from 'react-bootstrap';
 
 
 // --------------COMPONENTS-------------------//
 import ErrorBox from '../errorBox/ErrorBox';
 import DeleteModal from '../modals/deleteModal/DeleteModal';
-import EditModal from '../modals/editModal/EditModal';
 
 
 const Offs = () => {
@@ -17,15 +15,13 @@ const Offs = () => {
   const [allOffs , setAllOffs] = useState([])
   
   const [offID , setOffID] = useState(null)
+  const [isActive , setIsActive] = useState(null)
+
 
   const [isShowDeleteModal , setIsShowDeleteModal] = useState(false)
   const [isShowAcceptModal , setIsShowAcceptModal] = useState(false)
   const [isShowRejectModal , setIsShowRejectModal] = useState(false)
 
-  //states for EditModal inputs => bayad harkodam state joda dashte bashand
-  const [offNewPercent , setOffNewPercent] = useState('')
-  const [offNewCode , setOffNewCode] = useState('')
-  const [offNewDate , setOffNewDate] = useState('')
 
   // ------------ get all comments -----------//
   useEffect(() => {
@@ -61,6 +57,66 @@ const Offs = () => {
     })
   }
 
+  //for submit AcceptModal
+  const submitAcceptModal = () => {
+    fetch(`http://localhost:8000/api/offs/active-off/${offID}` , {
+      method:'PUT',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body:JSON.stringify({isActive})
+    
+    })
+    .then(res => {
+      res.json()
+      console.log(res)
+    })
+    .then((result) => {
+      console.log(result)
+      console.log(isActive);
+
+      setIsShowAcceptModal(false)
+      console.log('off ACCEPTED');
+      getAllOffs()
+    })
+
+  }
+  //for cancel AcceptModal
+  const cancelAcceptModal = () => {
+    setIsShowAcceptModal(false)
+    
+  }
+
+  //for submit RejecttModal
+  const submitRejectModal = () => {
+    fetch(`http://localhost:8000/api/offs/active-off/${offID}/${isActive}` , {
+      method:'PUT',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+    
+    })
+    .then(res => {
+      res.json()
+      console.log(res)
+    })
+    .then((result) => {
+      console.log(result)
+      console.log(isActive);
+      setIsShowRejectModal(false)
+      console.log('comment rejected');
+      getAllOffs()
+    })
+    .catch(err => console.log(err))
+
+  }
+   //for cancel RejectModal
+   const cancelRejectModal = () => {
+    setIsShowRejectModal(false)
+    
+  }
+
+
   return (
     <div className='offs'>
       {allOffs.length ? 
@@ -78,32 +134,34 @@ const Offs = () => {
               </tr>
             </thead>
             <tbody>
-              {allOffs.map((Off) => {
+              {allOffs.map((off) => {
                 return(
-                  <tr key={Off.id} className={Off.isActive ? ('e1') : ('e2')}>
-                    <td>{Off.id}</td>
-                    <td>{Off.adminID}</td>
-                    <td>{Off.productID}</td>
-                    <td>{Off.percent}</td>
-                    <td>{Off.code}</td>
-                    <td>{Off.date}</td>
+                  <tr key={off.id} className={off.isActive ? ('e1') : ('e2')}>
+                    <td>{off.id}</td>
+                    <td>{off.adminID}</td>
+                    <td>{off.productID}</td>
+                    <td>{off.percent}</td>
+                    <td>{off.code}</td>
+                    <td>{off.date}</td>
                     <td>
                       <button 
                         className='btn btn-danger btn-EDIT'
                         onClick={() => {
                           setIsShowDeleteModal(true)
-                          setOffID(Off.id)
+                          setOffID(off.id)
                         }}
                       >
                         حذف  
                       </button>
-                      {Off.isActive ? 
+                      {off.isActive ? 
                         (
                           <button 
                             className='btn btn-danger btn-EDIT'
                             onClick={() => {
                               setIsShowRejectModal(true)
-                              setOffID(Off.id)
+                              setOffID(off.id)
+                              setIsActive(off.isActive)
+
                             }}
                           >
                             رد
@@ -115,7 +173,8 @@ const Offs = () => {
                             className='btn btn-success btn-EDIT'
                             onClick={() => {
                               setIsShowAcceptModal(true)
-                              setOffID(Off.id)
+                              setOffID(off.id)
+                              setIsActive(off.isActive)
                             }}
                           >
                             تایید  
@@ -140,6 +199,25 @@ const Offs = () => {
           submitAction={submitDeleteModal} 
           cancelAction={cancelDeleteModal} 
           title='آیا از حذف اطمینان دارید ؟' 
+        />
+      )}
+
+      {/* ----------ACCEPT MODAL---------- */}
+      {/*for accepModal az DeleteModal estefadeh mikonim */}
+      {isShowAcceptModal && (
+        <DeleteModal
+          cancelAction={cancelAcceptModal}
+          submitAction={submitAcceptModal}
+          title='آیا از تایید اطمینان دارید ؟'
+        />
+      )}
+      {/* ----------REJECT MODAL---------- */}
+      {/*for RejectModal az DeleteModal estefadeh mikonim */}
+      {isShowRejectModal && (
+        <DeleteModal
+          cancelAction={cancelRejectModal}
+          submitAction={submitRejectModal}
+          title='آیا از رد اطمینان دارید ؟'
         />
       )}
 
